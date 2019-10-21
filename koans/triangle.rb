b44f8f0d3880fa0b7b6c800d3e4c3ce84d3f7259
Emily <delamern@gmail.com>
2019-10-21 11:19:18 -0500

    Added triangle exceptions -- 161/282

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,6 +14,12 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+  if(a <= 0 || b <= 0 || c <= 0)
+    raise TriangleError, "No side may be zero or less"
+  end
+  if( (a + b) <= c || (a + c) <= b || (b + c) <= a)
+    raise TriangleError, "Any two sides of a triangle should add up to more than the third side"
+  end
   if(a == b && a == c)
     :equilateral
   elsif(a == b || a == c || b == c)
